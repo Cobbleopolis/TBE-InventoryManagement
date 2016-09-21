ff78ba354e45b421125b882163c254f2e78c24ac
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-21 16:53:02 -0400

    More work on isbn look proof of concept

--- app/assets/javascripts/index.ts
+++ app/assets/javascripts/index.ts
@@ -1,14 +1,25 @@
 import {Book} from "./models/book";
 
 let book: Book;
+let info: JQuery;
 
 $(function () {
+    info = $("#info");
     book = new Book("someISBN", "This is a Book");
     if (window.location.hash) {
         var hash = window.location.hash;
         $(hash).modal('toggle');
     }
     $("#isbnSubmit").on("click", () => {
-
-    })
+        $.ajax({
+            type: 'GET',
+            url: `isbnLookup/${$('isbnInput').val()}`,
+            error(error: JQueryXHR, status: string, errorThrown: string) {
+                info.html("<p>No books found.</p>")
+            },
+            success(data: any) {
+                info.html(data)
+            }
+        })
+    });
 });
