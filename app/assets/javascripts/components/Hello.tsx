f2d67c81b585edf6841d874d5c222b66d214ecd4
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-20 22:53:48 -0400

    React work

--- app/assets/javascripts/components/Hello.tsx
+++ app/assets/javascripts/components/Hello.tsx
@@ -2,8 +2,28 @@ import * as React from "react";
 
 export interface HelloProps { compiler: string; framework: string; }
 
-export class Hello extends React.Component<HelloProps, {}> {
+export interface HelloState { someInput: string; }
+
+export class Hello extends React.Component<HelloProps, HelloState> {
+
+    state: HelloState = {someInput: 'TypeScript'};
+
+    constructor(props: any) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event: any) {
+        this.setState({someInput: event.target.value})
+    }
+
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return (
+            <div>
+                <h1>Hello from {this.state.someInput} and {this.props.framework}!</h1>
+                <input type="text" value={this.state.someInput} onChange={this.handleChange} />
+            </div>
+        );
     }
 }
