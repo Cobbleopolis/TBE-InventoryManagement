d7c5535b4937607b5fbd2d08a504fd8a9f079da0
Cobbleopolis <cobbleopolis@gmail.com>
2016-09-22 15:14:30 -0400

    info div now shows a loading icon

--- app/assets/javascripts/index.ts
+++ app/assets/javascripts/index.ts
@@ -1,27 +1,29 @@
-import {Book} from "./models/book";
+import {Book} from './models/book';
 
 let book: Book;
 export let info: JQuery;
+let loadingHTML = '<i class="fa fa-circle-o-notch fa-spin" style="font-size:48px"></i>';
 
 $(() => {
-    info = $("#info");
-    book = new Book("someISBN", "This is a Book");
+    info = $('#info');
+    book = new Book('someISBN', 'This is a Book');
     if (window.location.hash) {
         var hash = window.location.hash;
         $(hash).modal('toggle');
     }
-    $("#isbnSubmit").on("click", () => {
+    $('#isbnSubmit').on('click', () => {
         isbnLookup($('#isbnInput').val())
     });
 });
 
 export function isbnLookup(isbn: string): void {
+    info.html(loadingHTML);
     $.ajax({
         type: 'GET',
         url: `/isbnLookup/${isbn}`,
         dataType: 'html',
         error(error: JQueryXHR, status: string, errorThrown: string) {
-            console.error(status + " | " + errorThrown, error);
+            console.error(status + ' | ' + errorThrown, error);
             info.html(`<p>${error.responseText}</p>`);
         },
         success(data: any) {
